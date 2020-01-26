2138cbee9366453a2262be584b8a9a930a523454
Mickel <andersson.mickel@gmail.com>
2020-01-26 16:05:43 +0100

    Add typography.js and normalize.css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import "normalize.css"
 import "./Layout.sass"
 
 const Layout = ({ children }) => {
---
+++ src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from "typography"
+import Wordpress2016 from "typography-theme-wordpress-2016"
+
+const typography = new Typography(Wordpress2016)
+
+export default typography

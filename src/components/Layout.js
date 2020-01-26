241dd7ee6fc8c8cd9eed7b8752514a3e563aab12
Mickel <andersson.mickel@gmail.com>
2020-01-26 16:19:09 +0100

    Rename gatsby boilerplate components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
       <h1>{data.site.siteMetadata.title}</h1>
       <div
         style={{
-          margin: `0 auto`,
+          margin: `30px auto`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}

46f99ec45f54d184a4f6eda7a71f926ae6f3920c
Mickel <andersson.mickel@gmail.com>
2020-01-29 18:10:58 +0100

    Tweak 404 copy

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,13 @@ import SEO from "../components/SEO.js"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Not Found</h1>
+    <p>
+      The page you are looking for does not exist on this address. Please
+      double-check, triple-check and then contact me if you think something is
+      missing here!
+    </p>
+    <p>Sorry for the inconvenience.</p>
   </Layout>
 )
 

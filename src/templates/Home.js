901b6b1797ba549b98c468969417e1813eb5b3d4
Mickel <andersson.mickel@gmail.com>
2020-02-15 15:31:17 +0100

    Add links to /products and /articles on home

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -17,7 +17,9 @@ export const HomeTemplate = ({ title, heroText, content }) => (
     <HTMLContent content={content} />
 
     <div className={styles.header}>
-      <h3>Products</h3>
+      <h3>
+        <Link to="/products">Products</Link>
+      </h3>
       <Button Elem={Link} to="/products">
         <MdChevronRight /> All Products
       </Button>
@@ -25,7 +27,9 @@ export const HomeTemplate = ({ title, heroText, content }) => (
     <ArticleRoll limit={3} />
 
     <div className={styles.header}>
-      <h3>Articles</h3>
+      <h3>
+        <Link to="/articles">Articles</Link>
+      </h3>
       <Button Elem={Link} to="/articles">
         <MdChevronRight /> All Articles
       </Button>

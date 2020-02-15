0cdd369a240edc8dd953771e126517f6bbe96d50
Mickel <andersson.mickel@gmail.com>
2020-02-15 14:01:13 +0100

    Use SEO component instead of messing with Helmet

--- src/templates/About.js
+++ src/templates/About.js
@@ -1,20 +1,15 @@
 import React from "react"
-import Helmet from "react-helmet"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout.js"
 import HTMLContent from "../components/HTMLContent.js"
 import Content from "../components/Content.js"
+import SEO from "../components/SEO.js"
 
-export const AboutTemplate = ({
-  title,
-  content,
-  contentComponent,
-  helmet = "",
-}) => {
+export const AboutTemplate = ({ title, content, contentComponent }) => {
   const PostContent = contentComponent || Content
   return (
     <Layout>
-      {helmet}
+      <SEO title={title} />
       <h1>{title}</h1>
       <PostContent content={content} />
     </Layout>
@@ -23,23 +18,11 @@ export const AboutTemplate = ({
 
 const AboutPage = ({ data }) => {
   const { frontmatter, html } = data.markdownRemark
-  const { siteMetadata } = data.site
   return (
     <AboutTemplate
       title={frontmatter.title}
       content={html}
       contentComponent={HTMLContent}
-      helmet={
-        <Helmet>
-          <title>
-            {frontmatter.title} | {siteMetadata.title}
-          </title>
-          <meta
-            name="description"
-            content="Personal blog by Mickel Andersson."
-          />
-        </Helmet>
-      }
     />
   )
 }
@@ -48,11 +31,6 @@ export default AboutPage
 
 export const pageQuery = graphql`
   query AboutTemplate {
-    site {
-      siteMetadata {
-        title
-      }
-    }
     markdownRemark(frontmatter: { template: { eq: "About" } }) {
       html
       frontmatter {
--- src/templates/Articles.js
+++ src/templates/Articles.js
@@ -1,13 +1,16 @@
 import React from "react"
-import Helmet from "react-helmet"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout.js"
 import BlogRoll from "../components/BlogRoll.js"
 import HTMLContent from "../components/HTMLContent.js"
+import SEO from "../components/SEO.js"
 
-export const ArticlesTemplate = ({ title, content, helmet = "" }) => (
+export const ArticlesTemplate = ({ title, content }) => (
   <Layout>
-    {helmet}
+    <SEO
+      title={title}
+      description="Blog posts and articles by Ultimate Machine."
+    />
     <h1>{title}</h1>
     <HTMLContent content={content} />
     <BlogRoll />
@@ -16,35 +19,13 @@ export const ArticlesTemplate = ({ title, content, helmet = "" }) => (
 
 const ArticlesPage = ({ data }) => {
   const { frontmatter, html } = data.markdownRemark
-  const { siteMetadata } = data.site
-  return (
-    <ArticlesTemplate
-      title={frontmatter.title}
-      content={html}
-      helmet={
-        <Helmet>
-          <title>
-            {frontmatter.title} | {siteMetadata.title}
-          </title>
-          <meta
-            name="description"
-            content="Blog posts and articles made by Ultimate Machine."
-          />
-        </Helmet>
-      }
-    />
-  )
+  return <ArticlesTemplate title={frontmatter.title} content={html} />
 }
 
 export default ArticlesPage
 
 export const pageQuery = graphql`
   query Articles {
-    site {
-      siteMetadata {
-        title
-      }
-    }
     markdownRemark(frontmatter: { template: { eq: "Articles" } }) {
       html
       frontmatter {

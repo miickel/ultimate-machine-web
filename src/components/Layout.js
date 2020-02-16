250fded7017dcbf94d8c16474592c6bedbe737f6
Mickel <andersson.mickel@gmail.com>
2020-02-16 15:20:31 +0100

    Fix theme flicker bug

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,37 +6,26 @@ import Footer from './Footer.js'
 import SEO from './SEO.js'
 import Container from './Container.js'
 
-const DARK_SIDE = 'um-theme-dark'
+const isWindowThemeDark = () => window.__theme === 'theme--dark'
 
 const Layout = ({children}) => {
-  const [isDarkMode, setDarkMode] = useState()
+  const [isDarkMode, setDarkMode] = useState(isWindowThemeDark())
 
   useEffect(() => {
-    const detectDarkMode = () => {
-      const ls = localStorage.getItem(DARK_SIDE)
-      if (ls) return ls === 'true'
-      return (
-        window.matchMedia &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches
-      )
-    }
-    setDarkMode(detectDarkMode())
+    const checkTheme = () => setDarkMode(isWindowThemeDark())
+    window.onThemeChange = checkTheme
+    checkTheme()
+    return () => (window.onThemeChange = () => {})
   }, [])
-
-  useEffect(() => {
-    document.body.className = isDarkMode ? 'theme--dark' : 'theme--light'
-  }, [isDarkMode])
-
-  useEffect(() => {
-    localStorage.setItem(DARK_SIDE, isDarkMode)
-  }, [isDarkMode])
 
   return (
     <>
       <SEO title={null} />
       <Header
         isDarkMode={isDarkMode}
-        onDarkModeToggle={() => setDarkMode(!isDarkMode)}
+        onDarkModeToggle={() =>
+          window.setTheme(isDarkMode ? 'theme--light' : 'theme--dark')
+        }
       />
       <Container>
         <main>{children}</main>

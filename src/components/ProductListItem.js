52af3f159da313b6bd38248f4397678e72a31bc4
Mickel <andersson.mickel@gmail.com>
2020-02-16 17:27:46 +0100

    Remove unused stuff

--- src/components/ProductListItem.js
+++ src/components/ProductListItem.js
@@ -1,11 +1,5 @@
 import React from 'react'
 import {Link} from 'gatsby'
-import {kebabCase} from 'lodash'
-import Card from './Card.js'
-import ButtonList from './ButtonList.js'
-import Button from './Button.js'
-import TagList from './TagList.js'
-import PreviewCompatibleImage from './PreviewCompatibleImage.js'
 import ExternalLink from './ExternalLink.js'
 import styles from './ProductListItem.module.sass'
 
@@ -14,20 +8,10 @@ const ProductListItem = ({
   description,
   slug,
   url,
-  tags,
-  featuredImage,
   startDate,
   endDate,
   exitDescription,
 }) => {
-  const FeaturedImage = (
-    <PreviewCompatibleImage
-      imageInfo={{
-        image: featuredImage,
-        alt: `Ultimate Machine - ${title}`,
-      }}
-    />
-  )
   return (
     <li className={styles.item}>
       <div className={styles.content}>
@@ -35,7 +19,6 @@ const ProductListItem = ({
           <Link to={slug}>{title}</Link>
         </h2>
         <p>{description}</p>
-        {false && FeaturedImage}
         <footer className={styles.footer}>
           <div>
             {url && <ExternalLink href={url} prefix={`View “${title}” on `} />}

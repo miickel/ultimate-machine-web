e6340bf0d8a8234841359bb849aef73fe3176bef
Mickel <andersson.mickel@gmail.com>
2020-02-16 17:46:53 +0100

    Add "explore"-link on product details page

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import {Link} from 'gatsby'
 import Tag from './Tag.js'
 import styles from './TagList.module.sass'
 
 const TagList = ({tags, linkFn}) => {
   if (!tags || tags.length === 0) return null
-
-  const El = linkFn ? Link : 'span'
 
   const getProps = tag => (linkFn ? {to: linkFn(tag)} : {})
 

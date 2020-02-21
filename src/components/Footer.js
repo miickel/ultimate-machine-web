1445b6e695346d12ee69c49a3eee668e97b0df6b
Mickel <andersson.mickel@gmail.com>
2020-02-21 14:53:35 +0100

    Add mailto link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import Obfuscate from 'react-obfuscate'
 import Container from './Container.js'
 import LogoIcon from './LogoIcon.js'
 import styles from './Footer.module.sass'
@@ -11,19 +12,33 @@ const Footer = () => {
         <hr />
         <ul>
           <li>
-            <a href={`https://github.com/miickel/um-blog/tree/master/src`}>
+            <a
+              href={`https://github.com/miickel/um-blog/tree/master/src`}
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+            >
               Discuss on <strong>Twitter</strong>
             </a>
           </li>
           <li>
-            <a href={`https://github.com/miickel/um-blog/tree/master/src`}>
+            <a
+              href={`https://github.com/miickel/um-blog/tree/master/src`}
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+            >
               Edit on <strong>GitHub</strong>
             </a>
           </li>
           <li>
-            <a href={`https://github.com/miickel/um-blog/tree/master/src`}>
+            <Obfuscate
+              obfuscateChildren={false}
+              email="mickel@ultimatemachine.se"
+              headers={{
+                body: '\n\n\nSent via ultimatemachine.se',
+              }}
+            >
               Send an <strong>E-mail</strong>
-            </a>
+            </Obfuscate>
           </li>
         </ul>
         <Link to="/" title="Ultimate Machine">

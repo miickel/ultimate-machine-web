ef815744f44e415be6c4cfdbb68c480b93ad5735
Mickel <andersson.mickel@gmail.com>
2020-02-27 12:59:11 +0100

    Rename ArticleRoll -> ProductRoll

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link, graphql} from 'gatsby'
 import Layout from '../components/Layout.js'
 import BlogRoll from '../components/BlogRoll.js'
-import ArticleRoll from '../components/ArticleRoll.js'
+import ProductRoll from '../components/ProductRoll.js'
 import Button from '../components/Button.js'
 import HTMLContent from '../components/HTMLContent.js'
 import {MdChevronRight} from 'react-icons/md'
@@ -22,7 +22,7 @@ export const HomeTemplate = ({title, heroText, content}) => (
         <MdChevronRight /> All Products
       </Button>
     </div>
-    <ArticleRoll limit={3} />
+    <ProductRoll limit={3} />
 
     <div className={styles.header}>
       <h3>Articles</h3>

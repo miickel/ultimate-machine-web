2a2fac7368ba42e1b8bcd482b72863aceb11e2d7
Mickel <andersson.mickel@gmail.com>
2020-03-01 10:36:45 +0100

    Add some more Open Graph tags

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,23 +1,46 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, {useEffect, useState} from 'react'
 import Helmet from 'react-helmet'
 import {useStaticQuery, graphql} from 'gatsby'
 
-function SEO({description, lang, meta, title, socialImage}) {
+function SEO({
+  description = '',
+  lang = 'en',
+  meta = [],
+  title,
+  socialImage,
+  type = 'website',
+  pathname,
+  publishDate,
+  tags = [],
+}) {
+  const [fallbackPathname, setFallbackPathname] = useState()
+
+  useEffect(() => {
+    if (!pathname) {
+      setFallbackPathname(window.location.pathname)
+    }
+  }, [pathname])
+
   const {site} = useStaticQuery(
     graphql`
       query {
         site {
+          buildTime
           siteMetadata {
             title
             description
-            author
+            authorName
+            authorTwitter
+            authorUrl
             siteUrl
           }
         }
       }
     `
   )
+
+  const canonicalUrl = `${site.siteMetadata.siteUrl}${pathname ||
+    fallbackPathname}`
 
   const metaDescription = description || site.siteMetadata.description
 
@@ -35,43 +58,95 @@ function SEO({description, lang, meta, title, socialImage}) {
           name: 'og:image:height',
           content: socialImage.childImageSharp.fluid.presentationHeight,
         },
+        {
+          name: 'image',
+          content: `${site.siteMetadata.siteUrl}${socialImage.childImageSharp.fluid.src}`,
+        },
       ]
     : []
 
+  let ogAuthor = []
+
+  if (type === 'article') {
+    ogAuthor = [
+      {
+        name: 'article:publisher',
+        content: site.siteMetadata.siteUrl,
+      },
+      {
+        name: 'article:author',
+        content: site.siteMetadata.authorUrl,
+      },
+      {
+        name: 'article:published_time',
+        content: publishDate,
+      },
+      {
+        name: 'article:modified_time',
+        content: site.buildTime,
+      },
+      ...tags.map(tag => ({
+        name: 'article:tag',
+        content: tag,
+      })),
+    ]
+    if (tags.length !== 0) {
+      ogAuthor.push({
+        name: 'article:section',
+        content: tags[tags.length - 1],
+      })
+    }
+  }
+
   const metaTags = [
     {
-      name: `description`,
+      name: 'description',
       content: metaDescription,
     },
     {
-      property: `og:title`,
+      name: 'author',
+      content: site.siteMetadata.authorName,
+    },
+
+    // Open Graph
+    {
+      property: 'og:type',
+      content: type,
+    },
+    {
+      property: 'og:title',
       content: title,
     },
     {
-      property: `og:description`,
+      property: 'og:description',
       content: metaDescription,
     },
     {
-      property: `og:type`,
-      content: `website`,
+      property: 'og:site_name',
+      content: site.siteMetadata.title,
     },
     {
-      name: `twitter:card`,
+      property: 'og:locale',
+      content: lang,
+    },
+    {
+      property: 'og:url',
+      content: canonicalUrl,
+    },
+
+    // Twitter
+    {
+      name: 'twitter:card',
       content: ogMeta.length === 0 ? 'summary' : 'summary_large_image',
     },
     {
-      name: `twitter:creator`,
-      content: site.siteMetadata.author,
+      name: 'twitter:creator',
+      content: site.siteMetadata.authorTwitter,
     },
-    {
-      name: `twitter:title`,
-      content: title,
-    },
-    {
-      name: `twitter:description`,
-      content: metaDescription,
-    },
+
+    // Extras
     ...ogMeta,
+    ...ogAuthor,
     ...meta,
   ]
 
@@ -85,20 +160,6 @@ function SEO({description, lang, meta, title, socialImage}) {
       meta={metaTags}
     />
   )
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string,
-  socialImage: PropTypes.object,
 }
 
 export default SEO

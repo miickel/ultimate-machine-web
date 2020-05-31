1c82a124967743b9cb247017871c66bd480738ce
Mickel <andersson.mickel@gmail.com>
2020-05-31 19:03:37 +0200

    Remove logs

--- lambda/newsletter-subscribe.js
+++ lambda/newsletter-subscribe.js
@@ -8,9 +8,7 @@ const {NETLIFY_FUNCTIONS_ROOT, NEWSLETTER_LIST_ID} = process.env
 const listId = parseInt(NEWSLETTER_LIST_ID, 10)
 
 exports.handler = async (event, context) => {
-  console.log(event.body)
   const {email, name} = JSON.parse(event.body)
-  console.log(email)
 
   try {
     if (!validateEmail(email) || !name) throw 'invalid'
